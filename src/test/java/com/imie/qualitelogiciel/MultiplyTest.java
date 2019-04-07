d7c54062345a168e195a575786c23cd666aca699
noobzor <fegbarthas@gmail.com>
2019-04-07 15:30:51 +0200

    correction test multiply

--- src/test/java/com/imie/qualitelogiciel/MultiplyTest.java
+++ src/test/java/com/imie/qualitelogiciel/MultiplyTest.java
@@ -5,75 +5,75 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class MultiplyTest {
-  
+
   @Test
-  public final void MultiplyerTest1() {
-    assertTrue(2.0*2.0 == Multiply.multiplyer(2.0,2.0));
+  public final void multiplyerTest1() throws OutOfRangeException {
+    assertTrue(2.0 * 2.0 == Multiply.multiplyer(2.0, 2.0));
   }
 
   @Test
-  public final void MultiplyerTest2() {
-    assertTrue(-1.0*2.0 == Multiply.multiplyer(-1.0, 2.0));
+  public final void multiplyerTest2() throws OutOfRangeException {
+    assertTrue(-1.0 * 2.0 == Multiply.multiplyer(-1.0, 2.0));
   }
-  
+
   @Test
-  public final void MultiplyerTest3() {
-    assertTrue(2.0*-1.0 == Multiply.multiplyer(2.0, -1.0));
+  public final void multiplyerTest3() throws OutOfRangeException {
+    assertTrue(2.0 * -1.0 == Multiply.multiplyer(2.0, -1.0));
   }
-  
+
   @Test
-  public final void MultiplyerTest4() {
-    assertTrue(0.0*2.0 == Multiply.multiplyer(0.0, 2.0));
+  public final void multiplyerTest4() throws OutOfRangeException {
+    assertTrue(0.0 * 2.0 == Multiply.multiplyer(0.0, 2.0));
   }
-  
+
   @Test
-  public final void MultiplyerTest5() {
-    assertTrue(0.0*-1.0 == Multiply.multiplyer(0.0, -1.0));
+  public final void multiplyerTest5() throws OutOfRangeException {
+    assertTrue(0.0 * -1.0 == Multiply.multiplyer(0.0, -1.0));
   }
-  
+
   @Test
-  public final void MultiplyerTest6() {
-    assertTrue(2.0*0.0 == Multiply.multiplyer(2.0, 0.0));
+  public final void multiplyerTest6() throws OutOfRangeException {
+    assertTrue(2.0 * 0.0 == Multiply.multiplyer(2.0, 0.0));
   }
-  
+
   @Test
-  public final void MultiplyerTest7() {
-    assertTrue(-1.0*0.0 == Multiply.multiplyer(-1.0, 0.0));
+  public final void multiplyerTest7() throws OutOfRangeException {
+    assertTrue(-1.0 * 0.0 == Multiply.multiplyer(-1.0, 0.0));
   }
-  
+
   @Test
-  public final void MultiplyerTest8() {
-    assertTrue(-1.0*-1.0 == Multiply.multiplyer(-1.0, -1.0));
+  public final void multiplyerTest8() throws OutOfRangeException {
+    assertTrue(-1.0 * -1.0 == Multiply.multiplyer(-1.0, -1.0));
   }
-  
+
   @Test(expected = OutOfRangeException.class)
-  public final void MultiplyerTest9() {
-    assertTrue(Double.MAX_VALUE*Double.MAX_VALUE == Multiply.multiplyer(Double.MAX_VALUE, Double.MAX_VALUE));
+  public final void multiplyerTest9() throws OutOfRangeException {
+    assertTrue(Double.MAX_VALUE * Double.MAX_VALUE == Multiply.multiplyer(Double.MAX_VALUE, Double.MAX_VALUE));
   }
-  
+
   @Test
-  public final void MultiplyerTest10() {
-    assertTrue(Double.MAX_VALUE*0.0 == Multiply.multiplyer(Double.MAX_VALUE, 0.0));
+  public final void multiplyerTest10() throws OutOfRangeException {
+    assertTrue(Double.MAX_VALUE * 0.0 == Multiply.multiplyer(Double.MAX_VALUE, 0.0));
   }
-  
+
   @Test
-  public final void MultiplyerTest11() {
-    assertTrue(-1.0*Double.MAX_VALUE == Multiply.multiplyer(-1.0, Double.MAX_VALUE));
+  public final void multiplyerTest11() throws OutOfRangeException {
+    assertTrue(-1.0 * Double.MAX_VALUE == Multiply.multiplyer(-1.0, Double.MAX_VALUE));
   }
-  
+
   @Test(expected = OutOfRangeException.class)
-  public final void MultiplyerTest12() {
-    assertTrue(Double.MIN_VALUE*Double.MIN_VALUE == Multiply.multiplyer(Double.MIN_VALUE, Double.MIN_VALUE));
+  public final void multiplyerTest12() throws OutOfRangeException {
+    assertTrue(Double.MIN_VALUE * Double.MIN_VALUE == Multiply.multiplyer(Double.MIN_VALUE, Double.MIN_VALUE));
   }
-  
+
   @Test
-  public final void MultiplyerTest13() {
-    assertTrue(Double.MIN_VALUE*0.0 == Multiply.multiplyer(Double.MIN_VALUE, 0.0));
+  public final void multiplyerTest13() throws OutOfRangeException {
+    assertTrue(Double.MIN_VALUE * 0.0 == Multiply.multiplyer(Double.MIN_VALUE, 0.0));
   }
-  
-  @Test(expected = OutOfRangeException.class)
-  public final void MultiplyerTest14() {
-    assertTrue(-1.0*Double.MIN_VALUE == Multiply.multiplyer(-1.0, Double.MIN_VALUE));
+
+  @Test
+  public final void multiplyerTest14() throws OutOfRangeException {
+    assertTrue(-1.0 * Double.MIN_VALUE == Multiply.multiplyer(-1.0, Double.MIN_VALUE));
   }
 
 }
