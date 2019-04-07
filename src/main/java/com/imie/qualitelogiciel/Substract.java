217c5109cde7eff0d5a6829f6eac191150baa2b3
noobzor <fegbarthas@gmail.com>
2019-04-07 21:50:07 +0200

    correction algo substract + checkstyle, pmd

--- src/main/java/com/imie/qualitelogiciel/Substract.java
+++ src/main/java/com/imie/qualitelogiciel/Substract.java
@@ -2,7 +2,12 @@ package com.imie.qualitelogiciel;
 
 public class Substract {
 
-  public static Double substracter(Double a, Double b) {
-    return a-b;
+  public static Double substracter(final Double a, final Double b) throws OutOfRangeException {
+    if (a > 0 && b < 0 && (Double.MAX_VALUE - a) < b) {
+      throw new OutOfRangeException();
+    } else if (a < 0 && b > 0 && (Double.MAX_VALUE + a) <= Math.abs(b)) {
+      throw new OutOfRangeException();
+    }
+    return a - b;
   }
 }
